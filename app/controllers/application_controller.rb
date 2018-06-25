1d6fa4c073afee8f28078753795c5fbcbe3acad5
Nelson Figueroa <nelson@admins-iMac-4.local>
2018-06-25 11:34:30 -0700

    Added name fields to users, added controller before_action to permit fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  # To allow first_name and last_name to pass through
+  before_action :permit_names, if: :devise_controller?
+
+  protected
+
+  def permit_names
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_22_180057) do
+ActiveRecord::Schema.define(version: 2018_06_25_173019) do
 
   create_table "candies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", limit: 25, null: false
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 2018_06_22_180057) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
